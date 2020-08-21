fba1b3b4dbdb970b51f61e58618e8f3c477ba55e
marijana-fi <61113052+marijana-fi@users.noreply.github.com>
2020-08-21 11:43:09 +0200

    Updates

---
+++ precache-manifest.9ebfecf409bbd7eb7f6c2cee081c9624.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "852331426d8dc38844421e7f9a0cd5a1",
+    "url": "/intellrocket-app/index.html"
+  },
+  {
+    "revision": "365b660f44d57914ee48",
+    "url": "/intellrocket-app/static/css/2.1c34b939.chunk.css"
+  },
+  {
+    "revision": "17e1d81ca9e76e7129e7",
+    "url": "/intellrocket-app/static/css/main.fb512423.chunk.css"
+  },
+  {
+    "revision": "365b660f44d57914ee48",
+    "url": "/intellrocket-app/static/js/2.39dd2754.chunk.js"
+  },
+  {
+    "revision": "6afc5b8125e17650f562742ffbd045c9",
+    "url": "/intellrocket-app/static/js/2.39dd2754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17e1d81ca9e76e7129e7",
+    "url": "/intellrocket-app/static/js/main.5937f789.chunk.js"
+  },
+  {
+    "revision": "3b30ba79839aa619b110",
+    "url": "/intellrocket-app/static/js/runtime-main.9da19f5d.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/intellrocket-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/intellrocket-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/intellrocket-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/intellrocket-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/intellrocket-app/precache-manifest.9ebfecf409bbd7eb7f6c2cee081c9624.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/intellrocket-app/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
