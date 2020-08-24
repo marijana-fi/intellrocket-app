cc196c1d60abe0bcd187c47e097e1113a7768028
marijana-fi <61113052+marijana-fi@users.noreply.github.com>
2020-08-24 15:47:45 +0200

    Updates

---
+++ precache-manifest.b418993f152c989dcb91b17d40212f65.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2154d5624e51fca2f5f4f89f35a926f",
+    "url": "/intellrocket-app/index.html"
+  },
+  {
+    "revision": "0fe4ebf122870838f6ce",
+    "url": "/intellrocket-app/static/css/2.1c34b939.chunk.css"
+  },
+  {
+    "revision": "70925e48115ee6cab012",
+    "url": "/intellrocket-app/static/css/main.075d8b16.chunk.css"
+  },
+  {
+    "revision": "0fe4ebf122870838f6ce",
+    "url": "/intellrocket-app/static/js/2.f0acc622.chunk.js"
+  },
+  {
+    "revision": "6afc5b8125e17650f562742ffbd045c9",
+    "url": "/intellrocket-app/static/js/2.f0acc622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70925e48115ee6cab012",
+    "url": "/intellrocket-app/static/js/main.05d6382c.chunk.js"
+  },
+  {
+    "revision": "3b30ba79839aa619b110",
+    "url": "/intellrocket-app/static/js/runtime-main.9da19f5d.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/intellrocket-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/intellrocket-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/intellrocket-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/intellrocket-app/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dedbfe359aed3c89a85d0bf65d4a603b.js"
+  "/intellrocket-app/precache-manifest.b418993f152c989dcb91b17d40212f65.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/intellrocket-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
